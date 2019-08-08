60f330bb8043d431b8de4147703b66441588ffae
Mukhamat Jafar <jafar.mukhamat7@gmail.com>
2019-08-08 22:50:20 +0700

    tugas_21_js
    tuga sudah ok !!!

---
+++ tugas-21.js
@@ -0,0 +1,11 @@
+function panggilreplace(){
+
+	var kata = "Saya belajar masak nasi goreng";
+
+	console.log(kata);
+	console.log("");
+	console.log(kata.replace("masak", "makan"));
+
+
+}
+panggilreplace();
